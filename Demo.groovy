57e908174da2fd9bf385ddb7e2064690171e8677
Amanda Baker <t-ambak@microsoft.com>
2016-07-30 20:24:30 -0700

    Remove stuff thats unnecesary for the demo

--- Demo.groovy
+++ Demo.groovy
@@ -1,11 +1,10 @@
-
 package myPackage
 
 app = new Api()
-app.baseUrl = 'http://localhost'
+app.init('http://localhost', true)
 
 // Mongo app for testing
-def appProperties = [
+def mongoProperties = [
     appId: 'mongo',
     appCpus: 0.1,
     appMem: 128,
@@ -13,69 +12,23 @@ def appProperties = [
     appImage: 'mongo',
     appContainerPort: 27017,
     appHostPort: 0,
-    appServicePort: 0,
+    appServicePort: 10001,
     appHaproxyGroup: 'internal'
 ]
 
-    // removed from above:
-        // appCmd: 'env && sleep 300',
-        // appEnv: [
-        //     LD_LIBRARY_PATH: '/usr/local/lib/myLib',
-        //     HA_HE_HE_HA: '/usr/lol/no/thx'
-        // ],
-        // appAcceptResourceRoles: [
-        //     'role1', '*'
-        // ],
-        // appLabels: [
-        //     environment: 'staging'
-        // ]
-        
-// Test scaling by scaling up to <scaleUp> instances
-def scaleUp = 5 
-def groupProperties = [
-    [
-        appId: 'mongooble',
-        appCpus: 0.1,
-        appMem: 256,
-        appInstances: 1,
-        appImage: 'mongo',
-        appContainerPort: 0,
-        appHostPort: 0,
-        appServicePort: 0,
-        appHaproxyGroup: 'internal'
-    ],
-    [
-        appId: 'mongoblet',
-        appCpus: 0.1,
-        appMem: 256,
-        appInstances: 1,
-        appImage: 'mongo',
-        appContainerPort: 0,
-        appHostPort: 0,
-        appServicePort: 0,
-        appHaproxyGroup: 'internal',
-    ]
+def nodeProperties = [
+    appId: 'node-web-app',
+    appCpus: 0.1,
+    appMem: 128,
+    appInstances: 1,
+    appImage: 'amandabaker/node-web-app',
+    appContainerPort: 80,
+    appHostPort: 80,
+    appServicePort: 10000,
+    appHaproxyGroup: 'external'    
 ]
 
-// Deploy Mongo app set in properties above
-// app.deployApp(appProperties)
-
-// Wait to restart to Marathon doesn't get mad
-sleep(1000)
-
-// Restart Mongo app
-//app.restartApp(appProperties.appId)
-
-//sleep(5000)
-
-//app.scaleApp(appProperties.appId, scaleUp)
-
-//sleep(5000)
-
-// Delete Mongo app
-//app.destroyApp(appProperties.appId)
-
-app.deployApp(appProperties)
-app.deployLoadBalancer('external')
-app.deployLoadBalancer('internal')
+// Deploy a mongo and a node-web-app container, and loadBalancers if necessary
+app.deployApp (mongoProperties)
+app.deployApp (nodeProperties)
 
